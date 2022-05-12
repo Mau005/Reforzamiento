5df11b99906f849a24384d887ea29bae9b99735d
mau <mpino1701@gmail.com>
2022-05-12 01:34:26 -0400

    first commit

---
+++ PruebaIf.py
@@ -0,0 +1,13 @@
+x = 10
+
+if x > 10:
+    print("X es mayor a 10")
+elif x >= 20:
+    print("x es mayor igual a 20")
+elif x >= 30:
+    print("x es menor o igual a 30")
+
+if x >= -5:
+    print("Es menor a menos 5")
+
+print("Pepito le gusta jugar al patio")
